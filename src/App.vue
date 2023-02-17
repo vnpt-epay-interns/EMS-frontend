9b52118d0ac4c6be2fed61dc4f0e2796dd154889
hoangvythptlhp0710 <hoangvytb7891@gmail.com>
2023-02-17 15:38:52 +0700

    check header:

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,61 @@
 
 <template>
+    <div class="app">
+        <Sidebar />
 
-    <Header />
+        <router-view>
+
+        </router-view>
+    </div>
 </template>
+
 <script setup>
 
 import Header from './views/components/Header.vue'
-    
+import Sidebar from './views/components/Sidebar.vue'
+
+
 </script>
 
 
 
-<style scoped>
+<style lang="scss">
+:root {
+    ---primary: #4ade80;
+    --grey: #64748b;
+    --dark: #1e293b;
+    --dark-alt: #334155;
+    --sidebar-width: 300px;
+}
 
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background: var(--light);
+}
+
+button {
+    cursor: pointer;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+}
+
+.app {
+    display: flex;
+
+    main {
+        flex: 1 1 0;
+        padding: 2rem;
+        @media (max-width: 768px) {
+            padding: 6rem;
+        }
+    }
+
+}
 </style>
