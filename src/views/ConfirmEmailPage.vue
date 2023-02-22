b5d8358bcbc587c3564a24567462d154345289d1
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2023-02-22 17:07:41 +0700

    add confirm page

---
+++ src/views/ConfirmEmailPage.vue
@@ -0,0 +1,108 @@
+<script setup>
+    import Header from '../components/Header.vue'
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <Header />  
+            <div class="content">
+                <header>
+                    <div class="title">Enter Manager Code</div>
+                    <p class="description">Please fill your details to access your account.</p>
+                </header>
+    
+                <div class="body">
+                    <label for="code" class="login__container__body__form__group__label">Refenced Code</label>
+                    <input type="text" id="code" class="login__container__body__form__group__input" placeholder="Enter Here" v-model="email" />
+                </div>
+    
+                <footer>
+                    <button>Submit</button>
+                </footer>
+            </div>
+        </div>
+    </main>
+    
+</template>
+
+<style scoped>
+    main {
+        background-color: var(--primary);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        padding: 50px;
+    }
+
+    .container {
+        width: 35%;
+        height: 100%;
+        max-width: 800px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        background: #fff;
+        padding: 0 30px 0;
+    }
+
+    .title {
+        font-weight: 700;
+        font-size: 40px;
+        line-height: 48px;
+        letter-spacing: 0.01em;
+        text-transform: capitalize;
+    }
+
+    .description {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+        color: #667085;
+    }
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .body {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 30px;
+    }
+    
+    header {
+        margin-bottom: 56px;
+        font-style: normal;
+    }
+
+    footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    input {
+        border: 1px solid #D0D5DD;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    input:focus {
+        outline: none;
+    }
+
+    button {
+        cursor: pointer;
+        border-radius: 5px;
+        padding: 10px;
+        min-width: 176px;
+        height: 44px;
+        background-color: var(--primary);
+        color: #fff;
+        border: none;
+    }
+</style>
---
+++ src/views/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="logo">
+        <img src="../assets/logo/logo1.png" alt="">
+        <div class="name">
+            <h2>EMS</h2>
+            <h4>Employee Management System</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+.logo {
+    width: 35%;
+    max-width: 800px;   
+    display: flex;
+    gap: 10px;
+    width: fit-content;
+    margin: 10px auto 30px auto;     
+}
+
+img {
+    width: 50px;
+    height: auto;
+}
+
+.name {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+h2, h4{
+    margin: 0;
+    font-size: 10px;
+}
+</style>
